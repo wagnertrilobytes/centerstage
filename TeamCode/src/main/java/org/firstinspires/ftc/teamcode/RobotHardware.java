d572608a78a11eb88583b4574d15ad56bd9be2c4
trilobyte <39575591+sgutierrez8c54@users.noreply.github.com>
2023-10-10 17:55:14 -0500

    Init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class RobotHardware {
+    /* Public OpMode members. */
+    public DcMotor frontLeft = null;
+    public DcMotor frontRight = null;
+    public DcMotor backLeft = null;
+    public DcMotor backRight = null;
+    //public CRServo claw = null;
+    //public Servo clawR = null;
+    public DcMotor slideLeft = null;
+    public DcMotor slideRight = null;
+    //   public DcMotor armB = null;
+    public DcMotor plane = null;
+    public DcMotor intake = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+   // BNO055IMU imu;
+
+    private ElapsedTime period = new ElapsedTime();
+    /* Constructor */
+    public void test(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // Define and Initialize Motors
+        frontLeft = hwMap.get(DcMotor.class, "frontLeft");
+        frontRight = hwMap.get(DcMotor.class, "frontRight");
+        backLeft = hwMap.get(DcMotor.class, "backLeft");
+        backRight = hwMap.get(DcMotor.class, "backRight");
+        slideLeft = hwMap.get(DcMotor.class, "slideLeft");
+        slideRight = hwMap.get(DcMotor.class, "slideRight");
+        plane = hwMap.get(DcMotor.class, "plane");
+        intake = hwMap.get(DcMotor.class, "intake");
+
+/*
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+*/
+        frontLeft.setDirection (DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        slideLeft.setDirection(DcMotor.Direction.REVERSE);
+        slideRight.setDirection(DcMotor.Direction.FORWARD );
+
+        plane.setDirection(DcMotor.Direction.REVERSE);
+        //armB.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        slideLeft.setPower(0);
+        slideRight.setPower(0);
+        plane.setPower(0);
+        intake.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        plane.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //  armB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+       // claw = hwMap.get(CRServo.class, "claw");
+        //clawR = hwMap.get(Servo.class, "clawR");
+
+
+        //Brake Function
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        plane.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //    armB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,244 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.RobotHardware;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="CenterStage: Teleop", group="Linear Opmode")
+//@Disabled
+public class Teleop extends LinearOpMode {
+    RobotHardware robot = new RobotHardware();
+
+    // Declare OpMode mem bers.
+    //private ElapsedTime runtime = new ElapsedTime();
+    //private DcMotor leftDrive = null;
+    //private DcMotor rightDrive = null;
+
+
+
+    //@Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+       // robot.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        //runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //robot.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            double Speed = gamepad1.left_stick_y;
+            double Turn = gamepad1.left_stick_x;
+            double Strafe = gamepad1.right_stick_x;
+            double Slide = gamepad2.right_stick_y;
+            double Grab = gamepad2.left_stick_x;
+            double MAX_SPEED = 1.0;
+            double SLIDE_SPEED = 0.15;
+
+
+
+            double numFl = 0.45*Range.clip((+Speed - Turn - Strafe), -1, +1);
+            double numFr = 0.45*Range.clip((+Speed + Turn + Strafe), -1, +1);
+            double numBl = 0.35*Range.clip((+Speed + Turn - Strafe), -1, +1);
+            double numBr = 0.45*Range.clip((+Speed - Turn + Strafe), -1, +1);
+            double numUp = 0.10*Range.clip((-Slide), -1, +1);
+            double numGrab = Range.clip((+Grab), -1, +1);
+
+
+
+           /* if (gamepad2.b && rotation<2824)
+                robot.arm.setTargetPosition(1000);
+                robot.arm.getCurrentPosition();
+                if (robot.arm.)
+                robot.arm.setPower(.5);*/
+            //Fabian Bafoonery
+
+            //rotation values for height
+            // small:negative -1875
+            // medium: -3150
+            // tall height: -4200
+
+
+
+
+            // Show the elapsed game time and wheel power.
+            //telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            double frontLeftPower = robot.frontLeft.getPower();
+            double frontRightPower = robot.frontRight.getPower();
+            double backLeftPower = robot.backLeft.getPower();
+            double backRightPower = robot.backRight.getPower();
+            //double planePower = robot.plane.getPower();
+           // double armPower = robot.arm.getPower();
+           // telemetry.addData("Arm height:", robot.arm.getCurrentPosition());
+            //telemetry.addData("Motors Power", "frontLeft (%.2f), frontRight (%.2f), backLeft (%.2f), backRight (%.2f), arm (%.2f)", frontLeftPower, frontRightPower, backLeftPower,backRightPower, armPower);
+            telemetry.update();
+
+
+
+           // robot.arm.setPower(numUp - MAX_SPEED + MAX_SPEED);
+            //robot.armB.setPower(numUp - MAX_SPEED + MAX_SPEED);
+            robot.frontLeft.setPower(numFl - MAX_SPEED + MAX_SPEED);
+            robot.frontRight.setPower(numFr - MAX_SPEED + MAX_SPEED);
+            robot.backLeft.setPower(numBl - MAX_SPEED + MAX_SPEED);
+            robot.backRight.setPower(numBr - MAX_SPEED + MAX_SPEED);
+            robot.slideLeft.setPower(numUp - SLIDE_SPEED  + SLIDE_SPEED);
+            robot.slideRight.setPower(numUp - SLIDE_SPEED  + SLIDE_SPEED);
+
+            /*//OPEN
+            if (gamepad2.dpad_left){
+                robot.clawL.setPosition(0.47);
+                robot.clawR.setPosition(0.80);
+            }
+
+            //CLOSE
+            if (gamepad2.dpad_right){
+                robot.clawL.setPosition(0.52);
+                robot.clawR.setPosition(0.48);
+            }*/
+
+
+            //plane shooty
+            boolean shoot = true;
+            if(shoot == true)
+            {
+                while (gamepad2.left_bumper)
+                {
+                robot.plane.setPower(1);
+                shoot = false;
+                }
+                if (shoot == false)
+                {
+                    robot.plane.setPower(0);
+                }
+            }
+
+            //intake grabby
+            boolean grab = true;
+            if(grab == true)
+            {
+                while (gamepad2.dpad_down)
+                {
+                    robot.intake.setPower(1);
+                    grab = false;
+                }
+                 if (grab == false)
+                {
+                    robot.intake.setPower(0);
+                }
+            }
+            //outake spitty
+            if(grab == true)
+            {
+                while (gamepad2.dpad_up)
+                {
+                    robot.intake.setPower(-1);
+                    grab = false;
+                }
+                if (grab == false)
+                {
+                    robot.intake.setPower(0);
+                }
+            }
+
+            //vroom drivey
+            boolean vroom = true;
+            if(vroom == true)
+            {
+                while(gamepad1.dpad_down)
+                {
+                    robot.frontLeft.setPower(1);
+                    robot.frontRight.setPower(1);
+                    robot.backLeft.setPower(1);
+                    robot.backRight.setPower(1);
+                    vroom = false;
+                }
+                if(vroom == false)
+                {
+                    robot.frontLeft.setPower(0);
+                    robot.frontRight.setPower(0);
+                    robot.backLeft.setPower(0);
+                    robot.backRight.setPower(0);
+                }
+            }
+            boolean vroom2 = true;
+            if(vroom2 == true)
+            {
+                while(gamepad1.dpad_up)
+                {
+                    robot.frontLeft.setPower(-1);
+                    robot.frontRight.setPower(-1);
+                    robot.backLeft.setPower(-1);
+                    robot.backRight.setPower(-1);
+                    vroom2 = false;
+                }
+                if(vroom2 == false)
+                {
+                    robot.frontLeft.setPower(0);
+                    robot.frontRight.setPower(0);
+                    robot.backLeft.setPower(0);
+                    robot.backRight.setPower(0);
+                }
+            }
+
+
+            telemetry.update();
+        }
+    }
+}
