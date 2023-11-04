2c55a39a7ac6ab1259de18e9408475d369f9535d
trilobyte <39575591+sgutierrez8c54@users.noreply.github.com>
2023-11-04 08:23:27 -0500

    fix(replace) all code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,203 +1,113 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
+// import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
+@Disabled
 public class RobotHardware {
     /* Public OpMode members. */
-    private final ElapsedTime runtime = new ElapsedTime();
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-    public RobotHardware robot = this;
     public DcMotor frontLeft = null;
     public DcMotor frontRight = null;
     public DcMotor backLeft = null;
     public DcMotor backRight = null;
-    // public CRServo claw = null;
-    // public Servo clawR = null;
+    public CRServo claw = null;
+    //public Servo clawR = null;
     public DcMotor slideLeft = null;
     public DcMotor slideRight = null;
-    public Servo plane = null;
     public DcMotor intake = null;
-    public CRServo drop = null;
-//    public BNO055IMU imu = null;
-    public DcMotor[] motors;
-    public CRServo[] servos;
+    public Servo plane = null;
 
-
-    public LinearOpMode _opMode;
-    public HardwareMap _hwMap;
-    public Telemetry telemetry;
-    public boolean auto;
-
-    /* Math */
-    public double SLIDE_SPEED = 0.15;
+    public Servo hook = null;
+    /* local OpMode members. */
     HardwareMap hwMap = null;
-    // public BNO055IMU imu;
-    /* Initialize standard hardware interfaces */
-    public void init(HardwareMap ahwMap, LinearOpMode opMode, boolean initAuto) {
-        // Save reference to hardware map
-        this.robot = this;
-        this._opMode = opMode;
-        this._hwMap = ahwMap;
-        hwMap = ahwMap;
-        if (initAuto) this.auto = true;
+    // BNO055IMU imu;
 
-        this.telemetry = this._opMode.telemetry;
+    private ElapsedTime period = new ElapsedTime();
+    /* Constructor */
+    public void test(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
         // Define and Initialize Motors
-        // Wheels
         frontLeft = hwMap.get(DcMotor.class, "frontLeft");
         frontRight = hwMap.get(DcMotor.class, "frontRight");
         backLeft = hwMap.get(DcMotor.class, "backLeft");
         backRight = hwMap.get(DcMotor.class, "backRight");
-        // The slides
         slideLeft = hwMap.get(DcMotor.class, "slideLeft");
         slideRight = hwMap.get(DcMotor.class, "slideRight");
-        // The extra stuffs
-        plane = hwMap.get(Servo.class, "plane");
         intake = hwMap.get(DcMotor.class, "intake");
-        // We should probably make call this "drop" in the hardware map
-        drop = hwMap.get(CRServo. class, "claw");
-        motors = new DcMotor[]{frontLeft, frontRight, backLeft, backRight, slideLeft, slideRight, intake};
-        servos = new CRServo[]{ drop };
-//        servos.add(drop);
-//        imu = hwMap.get(BNO055IMU.class, "imu");
+        claw = hwMap.get(CRServo.class,"claw");
+        plane = hwMap.get(Servo.class, "plane");
+        hook = hwMap.get(Servo.class, "hook");
+        
 
-//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-//        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-//        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-//        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-//        parameters.loggingEnabled = true;
-//        parameters.loggingTag = "IMU";
-//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-//        imu.initialize(parameters);
+        //armB = hwMap.get(DcMotor.class, "armB");
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+   /*     BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;j
+       // parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    */
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+        slideLeft.setDirection(DcMotor.Direction.REVERSE);
+        slideRight.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        //armB.setDirection(DcMotor.Direction.REVERSE);
 
-        slideLeft.setDirection(DcMotor.Direction.FORWARD);
-        slideRight.setDirection(DcMotor.Direction.FORWARD);
+        // Set all motors to zero power
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        slideLeft.setPower(0);
+        slideRight.setPower(0);
+        intake.setPower(0);
 
-        plane.setDirection(Servo.Direction.REVERSE);
 
-        // Set all motors to zero power, run with encoders, and ZPB to brake
-        for (DcMotor e : motors)
-        {
-            e.setPower(0);
-            e.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            e.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        }
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER );
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        this.telemetry.addData("Fabian Bafoonery", "Fabian Bafoonery");
-        this.telemetry.update();
-    }
+        slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //claw = hwMap.get(CRServo.class, "claw");
+        //clawR = hwMap.get(Servo.class, "clawR");
 
-    public void addPowerOnButtonPress(boolean button, DcMotor motor, double pressPower, double releasePower) {
-        if (button) { motor.setPower(pressPower); } else { motor.setPower(releasePower); }
-    }
-    public void addPowerOnButtonPress(boolean button, CRServo servo, double pressPower, double releasePower) {
-        if (button) { servo.setPower(pressPower); } else { servo.setPower(releasePower); }
-    }
 
-    public void driveRobot(double Drive, double Turn, double strafe) {
-        // Combine drive and turn for blended motion.
-        double fl = 0.45*Range.clip((+Drive - Turn -strafe), -1, +1);
-        double fr = 0.45*Range.clip((+Drive + Turn +strafe), -1, +1);
-        double bl = 0.45*Range.clip((+Drive + Turn - strafe), -1, +1);
-        double br = 0.45*Range.clip((+Drive - Turn + strafe), -1, +1);
+        //Brake Function
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
 
-//        double fl = Drive + Turn + strafe;
-//        double fr = Drive - Turn - strafe;
-//        double bl = Drive + Turn - strafe;
-//        double br = Drive - Turn + strafe;
 
-        // Scale the values so neither exceed +/- 1.0
-
-        // Use existing function to drive both wheels.
-//        this.setAllPowerSpec(numFl, numFr, numBl, numBr);
-        this.setAllPowerSpec(fl, fr, bl, br);
-    }
-
-    public void setAllPowerSpec(double fl, double fr, double bl, double br) {
-        this.frontLeft.setPower(fl);
-        this.frontRight.setPower(fr);
-        this.backLeft.setPower(bl);
-        this.backRight.setPower(br);
-    }
-
-    public void encoderDrive(double speed,
-                             double leftInches,
-                             double rightInches,
-                             double timeoutS) {
-
-        // Ensure that the OpMode is still active
-        if (_opMode.opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            int leftCounted = (int)(leftInches * COUNTS_PER_INCH);
-            int rightCounted = (int)(rightInches * COUNTS_PER_INCH);
-            this.frontLeft.setTargetPosition(this.frontLeft.getCurrentPosition() + leftCounted);
-            this.backLeft.setTargetPosition(this.backLeft.getCurrentPosition() + leftCounted);
-            this.frontRight.setTargetPosition(this.frontRight.getCurrentPosition() + rightCounted);
-            this.backRight.setTargetPosition(this.backRight.getCurrentPosition() + rightCounted);
-
-            for (DcMotor e : this.motors)
-            {
-                e.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            for (DcMotor e : this.motors)
-            {
-                e.setPower(Math.abs(speed));
-            }
-
-            while (_opMode.opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (this.frontLeft.isBusy() && this.frontRight.isBusy()) &&
-                    (this.frontRight.isBusy() && this.frontLeft.isBusy())
-            ) {
-
-                // Display it for the driver.
-                this.telemetry.addData("Running to",  " %7d :%7d",
-                        this.frontLeft.getCurrentPosition() + leftCounted,
-                        this.frontRight.getCurrentPosition() + rightCounted);
-                this.telemetry.addData("Currently at",  " at fl%7d fr%7d bl%7d br%7d",
-                        this.frontLeft.getCurrentPosition(), this.frontRight.getCurrentPosition(),
-                        this.backLeft.getCurrentPosition(), this.backRight.getCurrentPosition());
-                this.telemetry.update();
-            }
-
-            // Stop all motion;
-            this.setAllPowerSpec(0,0,0,0);
-
-            // Turn off RUN_TO_POSITION
-            for (DcMotor e : this.motors)
-            {
-                e.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            }
-
-            this._opMode.sleep(250);   // optional pause after each move.
-        }
     }
 }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oldtest/Cooking.java
@@ -0,0 +1,206 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.oldtest;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.RobotHardware;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+
+/*
+ * This OpMode illustrates the basics of TensorFlow Object Detection, using
+ * the easiest way.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
+ */
+@TeleOp(name = "this should NOT work", group = "Robot")
+
+public class Cooking extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    static final double     FORWARD_SPEED = 0.5;
+    static final double     REVERSE_SPEED = -0.5;
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    static final double     COUNTS_PER_MOTOR_REV    = 384.5 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    /**
+     * The variable to store our instance of the TensorFlow Object Detection processor.
+     */
+    private TfodProcessor tfod;
+    RobotHardware robot = new RobotHardware();
+    /**
+     * The variable to store our instance of the vision portal.
+     */
+    private VisionPortal visionPortal;
+    private HardwareMap RobotHardware;
+
+    @Override
+    public void runOpMode() {
+
+        // i was lazy
+
+        robot.init(RobotHardware);
+
+        initTfod();
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+
+                telemetryTfod();
+
+                // Push telemetry to the Driver Station.
+                telemetry.update();
+
+                // a to stream; b to stop cause ye
+                if (gamepad1.b) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.a) {
+                    visionPortal.resumeStreaming();
+                }
+
+                // poor pc pt.2
+                sleep(20);
+            }
+        }
+
+        // doesnt kill the poor laptop
+        visionPortal.close();
+
+    }   // end runOpMode()
+
+    /**
+     * Initialize the TensorFlow Object Detection processor.
+     */
+    private void initTfod() {
+
+        //  TensorFlow processor the easy way
+        tfod = TfodProcessor.easyCreateWithDefaults();
+
+        // vision portal the easy way
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), tfod);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, tfod);
+        }
+
+    }   // end method initTfod()
+
+    /**
+     * Add telemetry about TensorFlow Object Detection (TFOD) recognitions.
+     */
+    private void telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+
+
+        //start of bullshit
+        robot.frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at", "%7d :%7d",
+                robot.frontRight.getCurrentPosition(),
+                robot.frontLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition());
+        telemetry.update();
+
+        waitForStart();
+
+        if (currentRecognitions.size() > 0) {
+            //fix the speeds because backwards :3
+            robot.frontLeft.setPower(FORWARD_SPEED);
+            robot.frontRight.setPower(REVERSE_SPEED);
+            robot.backLeft.setPower(FORWARD_SPEED);
+            robot.backRight.setPower(REVERSE_SPEED);
+            runtime.reset();
+            while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+                telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+                telemetry.update();
+            }
+
+            //bro ifd this shit works im actualy harvard material :pray_hands:
+            telemetry.addData("Path", "kys nigga");
+            telemetry.update();
+            sleep(1000);
+        }
+
+
+
+    }   // end method telemetryTfod()
+
+
+
+
+
+}   // end class
