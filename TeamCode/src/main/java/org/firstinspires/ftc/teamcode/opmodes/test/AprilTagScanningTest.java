c940265259b4ebc100653f495a9e6cca8bcf5aa4
trilobyte <39575591+sgutierrez8c54@users.noreply.github.com>
2024-01-09 08:32:05 -0600

    Cook up apriltag moveto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/AprilTagScanningTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/AprilTagScanningTest.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.opmodes.test;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
@@ -11,8 +13,10 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
-@Autonomous(name="April tag test!!!", group = "Tests")
+@TeleOp(name="April tag test!!!", group = "Tests")
+@Config
 public class AprilTagScanningTest extends LinearOpMode {
+    public static double WANTED_ID = 1;
     @Override
     public void runOpMode() {
         SampleMecanumDrive robot = new SampleMecanumDrive(hardwareMap);
@@ -33,9 +37,12 @@ public class AprilTagScanningTest extends LinearOpMode {
         }
         waitForStart();
         while (!isStopRequested() && opModeIsActive()) {
-            AprilTagDetection currTag = null;
             List<AprilTagDetection> currentDetections = aprilTags.getDetections();
+            double calculatedDist = 0;
+            double turn = 0;
             for (AprilTagDetection detection : currentDetections) {
+                if (detection.id != WANTED_ID) return;
+                AprilTagDetection currTag = detection;
                 // Look to see if we have size info on this tag.
                 telemetry.addData("Tag ID", detection.id);
                 telemetry.addData("x", detection.ftcPose.x);
@@ -47,37 +54,37 @@ public class AprilTagScanningTest extends LinearOpMode {
                 telemetry.addData("bearing", detection.ftcPose.bearing);
                 telemetry.addData("range", detection.ftcPose.range);
                 telemetry.addData("elevation", detection.ftcPose.elevation);
-            }
-            double calculatedDist = currTag.ftcPose.range - 5;
-            telemetry.addData("Gamepad1 A", "Go to that tag (move forward " + calculatedDist +"in)");
-            telemetry.addData("Gamepad1 B", "Face tag [BEARING] (" + currTag.ftcPose.bearing+"deg)");
-            telemetry.addData("Gamepad1 X", "Face tag [YAW] (" + currTag.ftcPose.yaw+"deg)");
-            telemetry.addData("Gamepad1 Y", "Face tag [PITCH] (" + currTag.ftcPose.pitch+"deg)");
-            telemetry.update();
+                calculatedDist = currTag.ftcPose.range - 5;
+                telemetry.addData("Gamepad1 A", "Go to that tag (move forward " + calculatedDist +"in)");
+                telemetry.addData("Gamepad1 B", "Face tag [BEARING] (" + currTag.ftcPose.bearing+"deg)");
+                telemetry.addData("Gamepad1 X", "Face tag [YAW] (" + currTag.ftcPose.yaw+"deg)");
+                telemetry.addData("Gamepad1 Y", "Face tag [PITCH] (" + currTag.ftcPose.pitch+"deg)");
+                telemetry.update();
 
-            if (gamepad1.a) {
-                TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
-                        .forward(calculatedDist)
-                        .build();
-                robot.followTrajectorySequenceAsync(trajsec);
-            }
-            if (gamepad1.b) {
-                TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
-                        .turn(Math.toRadians(currTag.ftcPose.bearing))
-                        .build();
-                robot.followTrajectorySequenceAsync(trajsec);
-            }
-            if (gamepad1.b) {
-                TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
-                        .turn(Math.toRadians(currTag.ftcPose.yaw))
-                        .build();
-                robot.followTrajectorySequenceAsync(trajsec);
-            }
-            if (gamepad1.b) {
-                TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
-                        .turn(Math.toRadians(currTag.ftcPose.pitch))
-                        .build();
-                robot.followTrajectorySequenceAsync(trajsec);
+                if (gamepad1.a) {
+                    TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
+                            .forward(calculatedDist)
+                            .build();
+                    robot.followTrajectorySequence(trajsec);
+                }
+                if (gamepad1.b) {
+                    TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
+                            .turn(Math.toRadians(currTag.ftcPose.bearing))
+                            .build();
+                    robot.followTrajectorySequence(trajsec);
+                }
+                if (gamepad1.x) {
+                    TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
+                            .turn(Math.toRadians(currTag.ftcPose.yaw))
+                            .build();
+                    robot.followTrajectorySequence(trajsec);
+                }
+                if (gamepad1.y) {
+                    TrajectorySequence trajsec = robot.trajectorySequenceBuilder(robot.getPoseEstimate())
+                            .turn(Math.toRadians(currTag.ftcPose.pitch))
+                            .build();
+                    robot.followTrajectorySequence(trajsec);
+                }
             }
         }
     }
